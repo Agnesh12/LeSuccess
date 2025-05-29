e0ff3d65ad58a699c2321fbb56bb12c881b8c4dd
AGNESH K <agnesh9025@gmail.com>
2025-05-29 21:54:40 +0530

    commit

---
+++ src/Assigment6/MajorityElement.java
@@ -0,0 +1,10 @@
+package Assigment6;
+
+import java.util.HashMap;
+
+public class MajorityElement {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 1, 1, 2, 2, 2, 3, 3};
+       HashMap<Integer, Integer> frequency = new HashMap<>();
+    }
+}
